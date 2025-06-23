1a2e32c0d9e250a3cfad5d428be5505c0a8c8b6c
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-06-23 22:19:02 +0530

    add code

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,16 +1,20 @@
-import { Routes, Route, Link } from 'react-router-dom'
-// import Home from './pages/Home'
-// import About from './pages/About'
-
-import Header from './components/Header/Header' 
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-       
-
+     
+      
     </>
   )
 }
+
+export default App
