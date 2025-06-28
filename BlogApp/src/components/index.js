a5ec9e10b1ed8f60da301b0af546fedfbcdc0270
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-06-28 11:01:59 +0530

    add LogoutBtn.jsx

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -3,7 +3,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn";
+import LogoutBtn from "./Header/LogOutBtn.jsx";
 import RTE from "./RTE.jsx";
 import Signup from "./Signup";
 import Login from "./Login";
